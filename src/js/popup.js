f9af5a1f9e42037c73e3b7948a00013f9fa8d300
Mogrima <rebel.rs@yandex.ru>
2018-10-01 12:59:48 +0500

    1. Сделала фокус на поля имя и почты в форме регистрации (не уверена, что это работает). 2. Привела названия var к единообразию. 3. Добавила закрытие модальных окон по щелчку на оверлей

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,22 @@
 var overlay = document.querySelector(".overlay");
+var storage = localStorage.getItem("user-name");
 
 var registrationLink = document.querySelector(".registration-link");
 var registrationForm = document.querySelector(".login__reg");
 var registrationClose = document.querySelector(".login__close-reg");
+var registrationlogin = registrationForm.querySelector("[name=user-name]");
+var registrationEmail = registrationForm.querySelector("[name=user-email]");
 
 registrationLink.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		registrationForm.classList.add("modal-show");
 		overlay.classList.add("modal-show");
+		if (storage) {
+			registrationlogin.value = storage;
+			registrationEmail.focus();
+		} else {
+			registrationlogin.focus();
+		}
 	});
 
 registrationClose.addEventListener("click", function(evt) {
@@ -30,19 +39,18 @@ window.addEventListener("keydown", function(evt) {
 var loginLink = document.querySelector(".login-link");
 var loginForm = document.querySelector(".login__log");
 var loginClose = document.querySelector(".login__close-log");
-var login = loginForm.querySelector("[name=user-name]");
-var password = loginForm.querySelector("[name=user-pass]");
-var storage = localStorage.getItem("user-name");
+var loginLogin = loginForm.querySelector("[name=user-name]");
+var loginPassword = loginForm.querySelector("[name=user-pass]");
 
 loginLink.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		loginForm.classList.add("modal-show");
 		overlay.classList.add("modal-show");
 		if (storage) {
-			login.value = storage;
-			password.focus();
+			loginLogin.value = storage;
+			loginPassword.focus();
 		} else {
-			login.focus();
+			loginLogin.focus();
 		}
 	});
 
@@ -61,4 +69,11 @@ window.addEventListener("keydown", function(evt) {
 				overlay.classList.remove("modal-show");
 			}
 		}
+	});
+
+overlay.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		registrationForm.classList.remove("modal-show");
+		loginForm.classList.remove("modal-show");
+		overlay.classList.remove("modal-show");
 	});
