28accf263f07d1578503443d69ddbfb664e9c680
Mogrima <rebel.rs@yandex.ru>
2018-10-30 19:54:02 +0500

    Добавила новый плагин для склейки скриптов. Добавила таск для отслеживания стилевых файлов читалки

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var autoprefixer = require("autoprefixer");
 var rigger = require("gulp-rigger");
 var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
+var concat = require("gulp-concat");
 var server = require("browser-sync").create();
 gulp.task("html", function () {
     gulp.src("src/**/*.html")
@@ -23,12 +24,29 @@ gulp.task("html", function () {
     .pipe(gulp.dest("dist/css"))
     .pipe(server.stream());
 });
+ gulp.task("book-css", function() {
+  gulp.src("src/less/book-rider/book-rider.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(server.stream());
+});
  gulp.task("js", function () {
     gulp.src("src/js/*.js")
         .pipe(uglify())
         .pipe(gulp.dest("dist/js"))
         .pipe(server.stream());
 });
+ gulp.task("concat-js", function () {
+  gulp.src("src/js/rider/*.js")
+        .pipe(uglify())
+        .pipe(concat("book.js"))
+        .pipe(gulp.dest("dist/js/"))
+        .pipe(server.stream());
+ })
  gulp.task("image", function () {
     gulp.src("src/img/**/*.*")
     .pipe(imagemin())
@@ -61,6 +79,9 @@ gulp.task("html", function () {
     });
   gulp.watch(["src/js/*.js"], function(event, cb) {
         gulp.start("js");
+    });
+  gulp.watch(["src/less/book-rider/*.less"], function(event, cb) {
+        gulp.start("book-css");
     });
   gulp.watch(["src/img/**/*.*"], function(event, cb) {
         gulp.start("image");
