a6fd1ea1902094242075fef1109864f8b472ba23
Mogrima <rebel.rs@yandex.ru>
2018-11-30 18:37:18 +0500

    Заменила галпфайл на новый, который взяла из седоны, чтобы быстрее вставлять рассказы

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 var gulp = require("gulp");
-var watch = require('gulp-watch');
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var rigger = require("gulp-rigger");
-var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
-var concat = require("gulp-concat");
 var server = require("browser-sync").create();
-gulp.task("html", function () {
-  gulp.src("src/**/*.html")
-        .pipe(rigger())
-        .pipe(gulp.dest("dist/"))
-        .pipe(server.stream());
+gulp.task("html:build", function () {
+    gulp.src("src/**/*.html") //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest("dist/")) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
  gulp.task("style", function() {
   gulp.src("src/less/style.less")
@@ -25,49 +22,28 @@ gulp.task("html", function () {
     .pipe(gulp.dest("dist/css"))
     .pipe(server.stream());
 });
- gulp.task("book", function() {
-  gulp.src("src/less/book.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("dist/css"))
-    .pipe(server.stream());
-});
- gulp.task("js", function () {
-    gulp.src("src/js/*.js")
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/js"))
-        .pipe(server.stream());
-});
- gulp.task("concat-js", function () {
-  gulp.src("src/js/rider/*.js")
-        .pipe(uglify())
-        .pipe(concat("book.js"))
-        .pipe(gulp.dest("dist/js/"))
-        .pipe(server.stream());
- })
- gulp.task("image", function () {
+ gulp.task("image:build", function () {
     gulp.src("src/img/**/*.*")
     .pipe(imagemin())
     .pipe(gulp.dest("dist/img"))
-    .pipe(server.stream());
+    .pipe(reload({stream: true}));
 });
- gulp.task("fonts", function() {
-    gulp.src("src/fonts/**/*.*")
+
+ gulp.task("book", function() {
+   gulp.src("src/less/book.less")
+     .pipe(plumber())
+     .pipe(less())
+     .pipe(postcss([
+       autoprefixer()
+     ]))
+     .pipe(gulp.dest("dist/css"))
+     .pipe(server.stream());
+ });
+ gulp.task("fonts:build", function() {
+    gulp.src("src/fonts/**/.*")
         .pipe(gulp.dest("dist/fonts"))
 });
- gulp.task("build", [
-    "html",
-    "js",
-    "style",
-    "book",
-    "fonts",
-    "image"
-]);
-
- gulp.task("serve", ["build"], function() {
+ gulp.task("serve", ["style"], function() {
   server.init({
     server: "dist/",
     notify: false,
@@ -76,20 +52,11 @@ gulp.task("html", function () {
     ui: false
   });
    gulp.watch("src/less/**/*.less", ["style"]);
-  gulp.watch("dist/**/*.html").on("change", server.reload);
+  gulp.watch("dist/**/.html").on("change", server.reload);
   gulp.watch(["src/**/*.html"], function(event, cb) {
-        gulp.start("html");
+        gulp.start("html:build");
     });
-  gulp.watch(["src/less/book.less"], function(event, cb) {
-        gulp.start("book");
-    });
-  gulp.watch(["src/js/*.js"], function(event, cb) {
-        gulp.start("js");
-    });
-  gulp.watch(["src/img/**/*.*"], function(event, cb) {
-        gulp.start("image");
-    });
-  gulp.watch(["src/fonts/**/*.*"], function(event, cb) {
-        gulp.start("fonts");
+  gulp.watch(["src/fonts/**/.*"], function(event, cb) {
+        gulp.start("fonts:build");
     });
 });
